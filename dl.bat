9beb3998a1680408bdb1577315ccc6b3d7f0e760
Jason Swift <anemele@outlook.com>
2025-11-21 16:26:56 +0800

    Initial commit

---
+++ dl.bat
@@ -0,0 +1,4 @@
+@echo off
+pushd %~dp0
+deno task dev
+popd
---
+++ main.ts
@@ -0,0 +1,90 @@
+import { ensureDir } from "@std/fs";
+import { join } from "@std/path";
+
+const OUT_DIR = "out";
+
+async function fetchM3U8(url: string): Promise<string[]> {
+  const resp = await fetch(url);
+  const text = await resp.text();
+  // await Deno.writeTextFile("play.m3u8", text);
+  const urls = text.trim().split("\n").filter((line) => !line.startsWith("#"));
+  return urls;
+}
+
+async function fetchOne(url: string, filepath: string) {
+  const resp = await fetch(url);
+  const arrayBuffer = await resp.arrayBuffer();
+  await Deno.writeFile(filepath, new Uint8Array(arrayBuffer));
+}
+
+async function hashsum(str: string): Promise<string> {
+  const hash = await crypto.subtle.digest(
+    "SHA-1",
+    new TextEncoder().encode(str),
+  );
+  return Array.from(new Uint8Array(hash))
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+}
+
+async function fetchAll(url: string) {
+  const cachePath = join(OUT_DIR, await hashsum(url));
+  await ensureDir(cachePath);
+
+  const urls = await fetchM3U8(url);
+
+  await Promise.all(urls.map((url, idx) => {
+    const filepath = join(
+      cachePath,
+      (idx + 1).toString().padStart(3, "0") + ".ts",
+    );
+    return fetchOne(url, filepath);
+  }));
+
+  const files = [] as string[];
+  Deno.readDirSync(cachePath).forEach((file) => {
+    files.push(`file '${file.name}'`);
+  });
+  const filesPath = join(cachePath, "files.txt");
+  await Deno.writeTextFile(filesPath, files.join("\n"));
+
+  const ffo = await new Deno.Command("ffmpeg", {
+    args: [
+      "-f",
+      "concat",
+      "-safe",
+      "0",
+      "-i",
+      filesPath,
+      "-c",
+      "copy",
+      `${cachePath}.mp4`,
+    ],
+  }).output();
+
+  if (!ffo.success) {
+    console.error("ffmpeg failed");
+    console.error(new TextDecoder().decode(ffo.stderr));
+    return;
+  }
+
+  await Deno.remove(cachePath, { recursive: true });
+}
+
+if (import.meta.main) {
+  console.log("m3u8dl <url>");
+  console.log("type nothing to exit");
+
+  while (true) {
+    console.log();
+    const url = prompt("url> ")?.trim();
+    if (!url) {
+      break;
+    }
+    try {
+      await fetchAll(url);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
